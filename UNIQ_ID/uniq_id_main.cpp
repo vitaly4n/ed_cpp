00017e9b9688e3dc0ba0070c44c995c2fe845e8b
vitaly4n <vitaly.4n@gmail.com>
2019-03-22 22:25:20 +0700

    implemented UNIQ_ID task, tests are OK

---
+++ UNIQ_ID/uniq_id_main.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+#include "uniq_id.cpp"
+
+int main() {
+  int UNIQ_ID = 0;
+  string UNIQ_ID = "hello";
+  vector<string> UNIQ_ID = {"hello", "world"};
+  vector<int> UNIQ_ID = {1, 2, 3, 4};
+}
