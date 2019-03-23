99e81f553e376ceff0edf659111bdb63d4a81293
vitaly4n <vitaly.4n@gmail.com>
2019-03-23 22:20:48 +0700

    clangformat profile.h

--- profile.h
+++ profile.h
@@ -8,21 +8,18 @@ using namespace std;
 using namespace std::chrono;
 
 class LogDuration {
-public:
+ public:
   explicit LogDuration(const string& msg = "")
-    : message(msg + ": ")
-    , start(steady_clock::now())
-  {
-  }
+      : message(msg + ": "), start(steady_clock::now()) {}
 
   ~LogDuration() {
     auto finish = steady_clock::now();
     auto dur = finish - start;
-    cerr << message
-       << duration_cast<milliseconds>(dur).count()
-       << " ms" << endl;
+    cerr << message << duration_cast<milliseconds>(dur).count() << " ms"
+         << endl;
   }
-private:
+
+ private:
   string message;
   steady_clock::time_point start;
 };
@@ -30,5 +27,4 @@ private:
 #define UNIQ_ID_IMPL(lineno) _a_local_var_##lineno
 #define UNIQ_ID(lineno) UNIQ_ID_IMPL(lineno)
 
-#define LOG_DURATION(message) \
-  LogDuration UNIQ_ID(__LINE__){message};
+#define LOG_DURATION(message) LogDuration UNIQ_ID(__LINE__){message};
