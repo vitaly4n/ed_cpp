9bb0fd1d5edfcdef5f91410050c717375af68659
vitaly4n <vitaly.4n@gmail.com>
2019-05-11 21:26:07 +0700

    secondary_index task: task description and working version

--- secondary_index/secondary_index.cpp
+++ secondary_index/secondary_index.cpp
@@ -1,9 +1,9 @@
 #include <algorithm>
-#include <cstdlib>
 #include <map>
 #include <set>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
@@ -16,42 +16,6 @@ struct Record
   int karma;
 
   bool operator==(const Record& other) const { return id == other.id; }
-};
-
-struct RecordIdLess
-{
-  using is_transparent = true_type;
-
-  bool operator()(const Record& lhs, const Record& rhs) const
-  {
-    return lhs.id < rhs.id;
-  }
-
-  bool operator()(const string& lhs, const Record& rhs) const
-  {
-    return lhs < rhs.id;
-  }
-
-  bool operator()(const Record& lhs, const string& rhs) const
-  {
-    return lhs.id < rhs;
-  }
-};
-
-struct RecordPointerUserHash
-{
-  size_t operator()(const Record* obj) const
-  {
-    return hash<string>{}(obj->user);
-  }
-};
-
-struct RecordPointerIdEqual
-{
-  bool operator()(const Record* lhs, const Record* rhs) const
-  {
-    return lhs->id == rhs->id;
-  }
 };
 
 class Database
@@ -71,25 +35,42 @@ public:
   void AllByUser(const string& user, Callback callback) const;
 
 private:
-  set<Record, RecordIdLess> primary_;
-  multimap<int, const Record*> secondary_timestamp_;
-  multimap<int, const Record*> secondary_karma_;
-  unordered_map<const Record*,
-                const Record*,
-                RecordPointerUserHash,
-                RecordPointerIdEqual>
-    secondary_user_;
+  using TimestampQuery = multimap<int, const Record*>;
+  using KarmaQuery = multimap<int, const Record*>;
+  using NameQuery = unordered_multimap<string_view, const Record*>;
+
+  struct RecordData
+  {
+    RecordData(const Record& record)
+      : record_{ record } {}
+
+    Record record_;
+    TimestampQuery::iterator timestamp_id_;
+    KarmaQuery::iterator karma_id_;
+    NameQuery::iterator name_id_;
+  };
+
+  using PrimaryIdQuery = unordered_map<string, RecordData>;
+
+  PrimaryIdQuery primary_query_;
+  TimestampQuery timestamp_query_;
+  KarmaQuery karma_query_;
+  NameQuery name_query_;
 };
 
 bool
 Database::Put(const Record& record)
 {
-  auto emplace_res = primary_.emplace(record);
+  auto emplace_res = primary_query_.emplace(record.id, record);
   if (emplace_res.second) {
-    const auto& inserted_record = *emplace_res.first;
-    secondary_timestamp_.emplace(inserted_record.timestamp, &inserted_record);
-    secondary_karma_.emplace(inserted_record.karma, &inserted_record);
-    secondary_user_.emplace(&inserted_record, &inserted_record);
+    auto& record_data = emplace_res.first->second;
+    auto& emplaced_record = record_data.record_;
+    record_data.timestamp_id_ =
+      timestamp_query_.emplace(emplaced_record.timestamp, &emplaced_record);
+    record_data.karma_id_ =
+      karma_query_.emplace(emplaced_record.karma, &emplaced_record);
+    record_data.name_id_ =
+      name_query_.emplace(emplaced_record.user, &emplaced_record); 
     return true;
   }
   return false;
@@ -98,28 +79,20 @@ Database::Put(const Record& record)
 const Record*
 Database::GetById(const string& id) const
 {
-  auto it = primary_.find(id);
-  return it != end(primary_) ? it.operator->() : nullptr;
+  auto it = primary_query_.find(id);
+  return it != end(primary_query_) ? &it->second.record_ : nullptr;
 }
 
 bool
 Database::Erase(const string& id)
 {
-  auto it = primary_.find(id);
-  if (it != end(primary_)) {
-    auto erase_secondary = [&id](const auto& key, auto& container) {
-      const auto eqrange = container.equal_range(key);
-      const auto it =
-        find_if(eqrange.first, eqrange.second, [&id](const auto& d) {
-          return d.second->id == id;
-        });
-      container.erase(it);
-    };
-
-    erase_secondary(it->timestamp, secondary_timestamp_);
-    erase_secondary(it->karma, secondary_karma_);
-    erase_secondary(it.operator->(), secondary_user_);
-    primary_.erase(it);
+  auto it = primary_query_.find(id);
+  if (it != end(primary_query_)) {
+    auto& record_data = it->second;
+    timestamp_query_.erase(record_data.timestamp_id_);
+    karma_query_.erase(record_data.karma_id_);
+    name_query_.erase(record_data.name_id_);
+    primary_query_.erase(it);
     return true;
   }
   return false;
@@ -129,11 +102,11 @@ template<typename Callback>
 void
 Database::RangeByTimestamp(int low, int high, Callback callback) const
 {
-  const auto first = secondary_timestamp_.lower_bound(low);
-  const auto last = secondary_timestamp_.upper_bound(high);
+  auto first = timestamp_query_.lower_bound(low);
+  auto last = timestamp_query_.upper_bound(high);
   for (auto it = first; it != last; ++it) {
-    if (!callback(*(it->second)))
-      break;
+    if (!callback(*it->second))
+      return;
   }
 }
 
@@ -141,11 +114,11 @@ template<typename Callback>
 void
 Database::RangeByKarma(int low, int high, Callback callback) const
 {
-  const auto first = secondary_karma_.lower_bound(low);
-  const auto last = secondary_karma_.upper_bound(high);
+  auto first = karma_query_.lower_bound(low);
+  auto last = karma_query_.upper_bound(high);
   for (auto it = first; it != last; ++it) {
-    if (!callback(*(it->second)))
-      break;
+    if (!callback(*it->second))
+      return;
   }
 }
 
@@ -153,13 +126,10 @@ template<typename Callback>
 void
 Database::AllByUser(const string& user, Callback callback) const
 {
-  Record support;
-  support.user = user;
-  auto bucket = secondary_user_.bucket(&support);
-  for (auto it = secondary_user_.begin(bucket);
-       it != secondary_user_.end(bucket);
-       ++it) {
-    if (!callback(*(it->second)))
-      break;
+  auto eqrange = name_query_.equal_range(user);
+  for (auto it = eqrange.first; it != eqrange.second; ++it)
+  {
+    if (!callback(*it->second))
+      return;
   }
 }
