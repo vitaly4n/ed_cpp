5e97bdfd1b8b11ba892c0ddcbc3e5fc29d90eda2
vitaly4n <vitaly.4n@gmail.com>
2019-07-15 22:23:48 +0700

    svg: firstshot

--- svg/svg.cpp
+++ svg/svg.cpp
@@ -1,10 +1,16 @@
-
 #include <iostream>
 #include <memory>
 #include <optional>
+#include <sstream>
 #include <string>
 #include <variant>
 #include <vector>
+
+#ifdef LOCAL_TEST
+
+#include "test_runner.h"
+
+#endif
 
 namespace Svg {
 
@@ -44,15 +50,22 @@ public:
   virtual ObjectPtr Accept(ObjectCloneVisitor& visitor) const = 0;
   virtual ObjectPtr Accept(ObjectMovingVisitor& visitor) = 0;
 
-  void Print(std::ostream& os) const;
-  virtual void PrintProperties(std::ostream& os) const;
+  virtual void Print(std::ostream& os) const;
+  virtual void PrintProperties(std::ostream& os) const = 0;
   virtual const std::string& GetName() const = 0;
+};
 
-  Object& SetFillColor(const Color& color);
-  Object& SetStrokeColor(const Color& color);
-  Object& SetStrokeWidth(double stroke_width);
-  Object& SetStrokeLineCap(const std::string& stroke_linecap);
-  Object& SetStrokeLineJoin(const std::string& stroke_linejoin);
+template<typename T>
+class BaseObject : public Object
+{
+public:
+  void PrintProperties(std::ostream& os) const override;
+
+  T& SetFillColor(const Color& color);
+  T& SetStrokeColor(const Color& color);
+  T& SetStrokeWidth(double stroke_width);
+  T& SetStrokeLineCap(const std::string& stroke_linecap);
+  T& SetStrokeLineJoin(const std::string& stroke_linejoin);
 
 private:
   Color fill_color_ = NoneColor;
@@ -62,7 +75,7 @@ private:
   std::optional<std::string> stroke_linejoin_;
 };
 
-class Circle : public Object
+class Circle : public BaseObject<Circle>
 {
 public:
   ObjectPtr Accept(ObjectCloneVisitor& visitor) const override;
@@ -79,7 +92,7 @@ private:
   double radius_ = 1.;
 };
 
-class Polyline : public Object
+class Polyline : public BaseObject<Polyline>
 {
 public:
   ObjectPtr Accept(ObjectCloneVisitor& visitor) const override;
@@ -94,12 +107,13 @@ private:
   std::vector<Point> points_;
 };
 
-class Text : public Object
+class Text : public BaseObject<Text>
 {
 public:
   ObjectPtr Accept(ObjectCloneVisitor& visitor) const override;
   ObjectPtr Accept(ObjectMovingVisitor& visitor) override;
 
+  void Print(std::ostream& os) const override;
   void PrintProperties(std::ostream& os) const override;
   const std::string& GetName() const override;
 
@@ -171,6 +185,27 @@ operator<<(std::ostream& os, const Point& pt)
   return os << pt.x << "," << pt.y;
 }
 
+constexpr auto QUOTE = '"';
+constexpr auto EQUALS = '=';
+
+template<typename T>
+void
+PrintProperty(std::ostream& os, std::string_view name, const T& val)
+{
+  os << name << EQUALS << QUOTE << val << QUOTE << " ";
+}
+
+template<typename It>
+void
+PrintProperty(std::ostream& os, std::string_view name, It first, It last, std::string_view delimiter = " ")
+{
+  os << name << EQUALS << QUOTE;
+  for (auto it = first; it != last; it = std::next(it)) {
+    os << *it << delimiter;
+  }
+  os << QUOTE << " ";
+}
+
 ///////////////////////////////////////////////////////
 // Object methods impl
 
@@ -181,41 +216,56 @@ Object::Print(std::ostream& os) const
   PrintProperties(os);
   os << "/>";
 }
+
+template<typename T>
 void
-Object::PrintProperties(std::ostream& os) const
+BaseObject<T>::PrintProperties(std::ostream& os) const
 {
-  // TODO
+  PrintProperty(os, "fill", fill_color_);
+  PrintProperty(os, "stroke", stroke_color_);
+  PrintProperty(os, "stroke-width", stroke_width_);
+  if (stroke_linecap_) {
+    PrintProperty(os, "stroke-linecap", *stroke_linecap_);
+  }
+  if (stroke_linejoin_) {
+    PrintProperty(os, "stroke-linejoin", *stroke_linejoin_);
+  }
 }
 
-Object&
-Object::SetFillColor(const Color& color)
+template<typename T>
+T&
+BaseObject<T>::SetFillColor(const Color& color)
 {
   fill_color_ = color;
-  return *this;
+  return static_cast<T&>(*this);
 }
-Object&
-Object::SetStrokeColor(const Color& color)
+template<typename T>
+T&
+BaseObject<T>::SetStrokeColor(const Color& color)
 {
   stroke_color_ = color;
-  return *this;
+  return static_cast<T&>(*this);
 }
-Object&
-Object::SetStrokeWidth(double stroke_width)
+template<typename T>
+T&
+BaseObject<T>::SetStrokeWidth(double stroke_width)
 {
   stroke_width_ = stroke_width;
-  return *this;
+  return static_cast<T&>(*this);
 }
-Object&
-Object::SetStrokeLineCap(const std::string& stroke_linecap)
+template<typename T>
+T&
+BaseObject<T>::SetStrokeLineCap(const std::string& stroke_linecap)
 {
   stroke_linecap_ = stroke_linecap;
-  return *this;
+  return static_cast<T&>(*this);
 }
-Object&
-Object::SetStrokeLineJoin(const std::string& stroke_linejoin)
+template<typename T>
+T&
+BaseObject<T>::SetStrokeLineJoin(const std::string& stroke_linejoin)
 {
   stroke_linejoin_ = stroke_linejoin;
-  return *this;
+  return static_cast<T&>(*this);
 }
 
 ///////////////////////////////////////////////////////
@@ -235,8 +285,11 @@ Circle::Accept(ObjectMovingVisitor& visitor)
 void
 Circle::PrintProperties(std::ostream& os) const
 {
-  // TODO
-  Object::PrintProperties(os);
+  PrintProperty(os, "cx", center_.x);
+  PrintProperty(os, "cy", center_.y);
+  PrintProperty(os, "r", radius_);
+
+  BaseObject::PrintProperties(os);
 }
 const std::string&
 Circle::GetName() const
@@ -275,8 +328,9 @@ Polyline::Accept(ObjectMovingVisitor& visitor)
 void
 Polyline::PrintProperties(std::ostream& os) const
 {
-  // TODO
-  Object::PrintProperties(os);
+  PrintProperty(os, "points", points_.begin(), points_.end());
+
+  BaseObject::PrintProperties(os);
 }
 const std::string&
 Polyline::GetName() const
@@ -307,10 +361,26 @@ Text::Accept(ObjectMovingVisitor& visitor)
 }
 
 void
+Text::Print(std::ostream& os) const
+{
+  os << "<" << GetName() << " ";
+  PrintProperties(os);
+  os << ">" << data_ << "</" << GetName() << ">";
+}
+
+void
 Text::PrintProperties(std::ostream& os) const
 {
-  // TODO
-  Object::PrintProperties(os);
+  PrintProperty(os, "x", position_.x);
+  PrintProperty(os, "y", position_.y);
+  PrintProperty(os, "dx", offset_.x);
+  PrintProperty(os, "dy", offset_.y);
+  PrintProperty(os, "font-size", font_size_);
+  if (font_family_) {
+    PrintProperty(os, "font-family", *font_family_);
+  }
+
+  BaseObject::PrintProperties(os);
 }
 const std::string&
 Text::GetName() const
@@ -410,16 +480,64 @@ Document::Add(const Object& object)
 void
 Document::Render(std::ostream& os)
 {
+  os << R"(<?xml version="1.0" encoding="UTF-8" ?>)";
+  os << R"(<svg xmlns="http://www.w3.org/2000/svg" version="1.1">)";
+
   for (const auto& object : objects_) {
     object->Print(os);
   }
+
+  os << R"(</svg>)";
 }
 
 } // namespace Svg
 
 #ifdef LOCAL_TEST
+
+void
+simple_test()
+{
+  Svg::Document svg;
+
+  svg.Add(Svg::Polyline{}
+            .SetStrokeColor(Svg::Rgb{ 140, 198, 63 }) // soft green
+            .SetStrokeWidth(16)
+            .SetStrokeLineCap("round")
+            .AddPoint({ 50, 50 })
+            .AddPoint({ 250, 250 }));
+
+  for (const auto& point : { Svg::Point{ 50, 50 }, Svg::Point{ 250, 250 } }) {
+    svg.Add(Svg::Circle{}.SetFillColor("white").SetRadius(6).SetCenter(point));
+  }
+
+  svg.Add(Svg::Text{}
+            .SetPoint({ 50, 50 })
+            .SetOffset({ 10, -10 })
+            .SetFontSize(20)
+            .SetFontFamily("Verdana")
+            .SetFillColor("black")
+            .SetData("C"));
+  svg.Add(Svg::Text{}
+            .SetPoint({ 250, 250 })
+            .SetOffset({ 10, -10 })
+            .SetFontSize(20)
+            .SetFontFamily("Verdana")
+            .SetFillColor("black")
+            .SetData("C++"));
+
+  std::string ref =
+    R"xx(<?xml version="1.0" encoding="UTF-8" ?><svg xmlns="http://www.w3.org/2000/svg" version="1.1"><polyline points="50,50 250,250 " fill="none" stroke="rgb(140,198,63)" stroke-width="16" stroke-linecap="round" /><circle cx="50" cy="50" r="6" fill="white" stroke="none" stroke-width="1" /><circle cx="250" cy="250" r="6" fill="white" stroke="none" stroke-width="1" /><text x="50" y="50" dx="10" dy="-10" font-size="20" font-family="Verdana" fill="black" stroke="none" stroke-width="1" >C</text><text x="250" y="250" dx="10" dy="-10" font-size="20" font-family="Verdana" fill="black" stroke="none" stroke-width="1" >C++</text></svg>)xx";
+  std::stringstream out;
+  svg.Render(out);
+  ASSERT_EQUAL(out.str(), ref);
+}
+
 int
 main(int, char*[])
-{}
+{
+  TestRunner tr;
+  RUN_TEST(tr, simple_test);
+  return 0;
+}
 
 #endif
